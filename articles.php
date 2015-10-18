dd1d43559f24c25c027b1d850ff06dc9468d7fcd
melifes <zhanglin-pc@live.com>
2015-10-18 08:25:39 +0800

    Refactor Less

--- articles.php
+++ articles.php
@@ -18,23 +18,23 @@ $this->widget('Widget_Contents_Post_Recent', 'pageSize=10000')->to($archives);
     $output = '<div id="archives">';   
     while($archives->next()):   
         $year_tmp = date('Y',$archives->created);   
-        $mon_tmp = date('F',$archives->created);
+        $mon_tmp = date('n',$archives->created);
         $day_tmp = date('d',$archives->created);  
         $y=$year; $m=$mon;   
         // if ($mon != $mon_tmp && $mon > 0) $output .= '</ul></li>';   
         if ($year != $year_tmp && $year > 0) $output .= '</ul>';   
         if ($year != $year_tmp) {   
             $year = $year_tmp;   
-            $output .= '<h3 class="al_year">'. $year .' 年</h3><ul class="articles-ul">'; //输出年份   
+            $output .= '<details><summary><h3 class="al_year">'. $year .' 年</h3></summary><ul class="articles-ul">'; //输出年份   
         }   
         // if ($mon != $mon_tmp) {   
             $mon = $mon_tmp;   
             // $output .= '<li><span class="al_mon">'. $mon .' 月</span><ul class="al_post_list">'; //输出月份   
         // } 
             $day = $day_tmp;  
-        $output .= '<li><a href="'.$archives->permalink .'">'. $archives->title .'</a>&nbsp;&nbsp;[<time>'.$mon.' '.$day.','.$year.'</time>]</li>'; //输出文章日期和标题   
+        $output .= '<li><a href="'.$archives->permalink .'">'. $archives->title .'</a>&nbsp;&nbsp;[<time>'.$mon.'月'.$day.'日'.'</time>]</li>'; //输出文章日期和标题   
     endwhile;   
-    $output .= '</ul></li></div>';   
+    $output .= '</ul></details></li></div>';   
     echo $output;   
 ?>
         </section>
--- index.php
+++ index.php
@@ -15,17 +15,23 @@ $this->need('header.php');
 	<div class="site-contentContainer">
 		<main class="site-content">
 			<h1 class="page-title">最近的文章</h1>
-			<ul class="articlelist">
+			<article class="articlelist">
 			<?php while($this->next()): ?>
-				<li class="articlelist-item">
+				<section class="articlelist-item">
 					<a href="<?php $this->permalink() ?>" class="article-view">
 					<h2 class="article-view-title"><?php $this->title() ?></h2>
-					<time class="article-view-date" datetime="<?php $this->date('c') ?>"><?php $this->date('F j, Y') ?></time>
-					<p class="article-view-excerpt"><?php $this->excerpt('200', '...') ?></p>
 					</a>
-				</li>
+					<time class="article-view-date" datetime="<?php $this->date('c') ?>"><?php $this->date('F j, Y') ?></time>
+					 
+					<div class="article-view-excerpt"><?php $this->excerpt('400', '...') ?></div>
+					<div class="post-tag">
+					<ul>
+        				<li><strong>></strong><?php $this->tags('</li><li>', true, '<a>None</a>') ?></li>
+       				</ul>
+					<a href="#" class="post-foot-link fr">阅读更多</a>
+				</section> 
 			<?php endwhile; ?>
-			</ul>
+			</article>
 			<nav class="page-nav"><?php $this->pageNav('«', '»'); ?></nav>
 		</main>
 	</div>
